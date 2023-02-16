fb2dea7d95ef9db99ecde2fdbcb5467b00a307a5
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-02-17 00:56:47 +0900

    feat: 방명록 기능 완성

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,7 +1,7 @@
 import movies from "./movies.js";
 
 const checkBox = (rank, title) => (
-  `<input type="checkbox" name="${title}" value="${title}">${title}</input>`
+  `<input type="checkbox" name="checkbox" value="${title}">${title}</input>`
 )
 
 const makeMovieCheckBox = () => {
@@ -9,6 +9,33 @@ const makeMovieCheckBox = () => {
   element.innerHTML = movies.map((m) => checkBox(m.rank, m.title)).join('');
 }
 
+const alertGuestCheck = () => {
+  const name = document.getElementById('name');
+  const selected = document.querySelectorAll('input[name="checkbox"]:checked')
+  alert(`${name.value}님, 저와 ${selected.length}개의 취향이 같으시네요!`)
+}
+
+const addEventListenerToAlertGuestButton = () => {
+  document.getElementById("alertGuestButton").addEventListener("click", () => {
+    alertGuestCheck();
+  });
+}
+
+const initAll = () => {
+  const name = document.getElementById('name');
+  name.value = ''
+  const selected = document.querySelectorAll('input[name="checkbox"]:checked')
+  selected.forEach(el => { el.checked = false })
+}
+
+const addEventListenerToCancelButton = () => {
+  const element = document.getElementById("cancelButton").addEventListener("click", () => {
+    initAll();
+  });
+}
+
 export const initCheckbox = () => {
   makeMovieCheckBox();
+  addEventListenerToAlertGuestButton();
+  addEventListenerToCancelButton();
 }
